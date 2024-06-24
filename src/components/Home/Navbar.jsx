09dc2f3a85ff556324c1a3835034b6d7febb00ba
sony-abc <132626593+sony-abc@users.noreply.github.com>
2024-06-25 01:07:00 +0530

    Update Navbar.jsx

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -12,26 +12,6 @@ function Navbar() {
     setNavActive(false);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 500) {
-        closeMenu;
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (window.innerWidth <= 1200) {
-      closeMenu;
-    }
-  }, []);
-
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
       <div>
